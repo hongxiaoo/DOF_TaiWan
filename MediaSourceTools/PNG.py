6717be1da510fa32ba6443c11584c33154c09759
zihunyeyu <39197422+zihunyeyu@users.noreply.github.com>
2019-08-05 04:39:30 +0800

    PNG模块调整

--- MediaSourceTools/PNG.py
+++ MediaSourceTools/PNG.py
@@ -1,64 +1,38 @@
-'''
+"""
 IMG内部PNG图片操作类模块
-'''
+"""
 from NkpImgTools import *
 from PIL import Image
-# 0x10（ARGB8888）
-# 0x0F（ARGB4444）
-# 0x0E（ARGB1555）
-
-
 v2ColorSystem = {16: 'ARGB8888', 15: 'ARGB4444', 14: 'ARGB1555'}
 
 
-class Png(object):
-
-    def _readPng(self):
-        pass
-
-    def _pngAna(self, pngAnaContent):
+class Pic:
+    def __init__(self, pngAnaContent):
         self.colorSystem = v2ColorSystem[pngAnaContent[0]]
         self.width = pngAnaContent[2]
         self.height = pngAnaContent[3]
-        self.decomPicC = pngAnaContent[-1]
+        self.content = pngAnaContent[-1]
+        self.img = Image.new('RGBA', (self.width, self.height))
+        self.draw()
 
+    def put_pixels(self, pixels, imageObj):
+        for i in range(self.width):
+            for j in range(self.height):
+                pixel_index = j * self.width + i
+                imageObj.putpixel((i, j), pixels[pixel_index])
 
-    def _saveImgPng(self):
-        imgPng = Image.new('RGBA', (self.width, self.height))
+    def draw(self):
+        # print(self.width * self.height)
         if self.colorSystem == 'ARGB8888':
-            k = 0
-            for i in range(self.width):
-                for j in range(self.height):
-                    imgPng.putpixel((i, j), (self.decomPicC[0 + 4 * k], self.decomPicC[1 + 4 * k],
-                                           self.decomPicC[2 + 4 * k], self.decomPicC[3 + 4 * k]))
-                    k += 1
-        imgPng.save('test.png')
-        imgPng.show()
+            self.put_pixels(argb8888(self.content), self.img)
+        elif self.colorSystem == 'ARGB4444':
+            self.put_pixels(argb4444(self.content), self.img)
+        elif self.colorSystem == 'ARGB1555':
+            self.put_pixels(argb1555(self.content), self.img)
+
+    def show(self):
+        self.img.show()
 
 
 if __name__ == '__main__':
-
-    image = Image.open('D:\\UserData\\Desktop\\test\\0.png', 'r')
-    print(image.size, image.format, image.mode)
-    image.show()
-    # img = Image.new('RGBA', (28, 28))
-    # img.show()
-    # a = []
-    # for i in range(image.size[0]):
-    #     for j in range(image.size[1]):
-    #         a.append(image.getpixel((i, j)))
-    # pixelindex = 0
-    # image2 = Image.new('RGBA', (164, 202))
-    # for i in range(image.size[0]):
-    #     for j in range(image.size[1]):
-    #         image2.putpixel((i, j), (a[pixelindex][0], a[pixelindex][1], a[pixelindex][2], a[pixelindex][3]))
-    #         if image.getpixel((i, j)) != image2.getpixel((i, j)):
-    #             print(image.getpixel((i, j)))
-    #             print(image2.getpixel((i, j)))
-    #         pixelindex += 1
-    # image2.save('test.png')
-
-
-
-
-
+    pass
